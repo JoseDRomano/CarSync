26068188bfafb6e3c881b8b45853aa34fb2aeece
JoseDRomano <j.romano@campus.fct.unl.pt>
2023-08-02 15:24:29 +0100

    sql ATUALIZADO

--- projeto_imt.sql
+++ projeto_imt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 02-Ago-2023 às 14:39
+-- Tempo de geração: 02-Ago-2023 às 15:50
 -- Versão do servidor: 10.4.28-MariaDB
 -- versão do PHP: 8.2.4
 
@@ -28,11 +28,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `customer` (
-  `driver_license_number` int(8) NOT NULL,
-  `license_type` char(1) NOT NULL,
-  `start_date` date NOT NULL,
-  `expiry_date` date NOT NULL,
-  `nif` int(9) NOT NULL
+                            `driver_license_number` int(8) NOT NULL,
+                            `license_type` char(1) NOT NULL,
+                            `start_date` date NOT NULL,
+                            `expiry_date` date NOT NULL,
+                            `nif` int(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -40,7 +40,7 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`driver_license_number`, `license_type`, `start_date`, `expiry_date`, `nif`) VALUES
-(1235566766, '1', '2023-08-10', '2025-08-15', 234554343);
+    (1235566766, '1', '2023-08-10', '2025-08-15', 234554343);
 
 -- --------------------------------------------------------
 
@@ -49,8 +49,8 @@ INSERT INTO `customer` (`driver_license_number`, `license_type`, `start_date`, `
 --
 
 CREATE TABLE `employee` (
-  `nif` int(9) NOT NULL,
-  `access_level` int(1) NOT NULL
+                            `nif` int(9) NOT NULL,
+                            `access_level` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -60,12 +60,12 @@ CREATE TABLE `employee` (
 --
 
 CREATE TABLE `insurance` (
-  `policy` int(13) NOT NULL,
-  `expiry_date` date NOT NULL,
-  `company` varchar(45) NOT NULL,
-  `start_date` date NOT NULL,
-  `extra_category` int(1) NOT NULL,
-  `plate` char(8) NOT NULL
+                             `policy` int(13) NOT NULL,
+                             `expiry_date` date NOT NULL,
+                             `company` varchar(45) NOT NULL,
+                             `start_date` date NOT NULL,
+                             `extra_category` int(1) NOT NULL,
+                             `plate` char(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -73,7 +73,7 @@ CREATE TABLE `insurance` (
 --
 
 INSERT INTO `insurance` (`policy`, `expiry_date`, `company`, `start_date`, `extra_category`, `plate`) VALUES
-(55476468, '2025-08-08', 'AGEAS Seguros', '2023-08-10', 1, '13-24-GD');
+    (55476468, '2025-08-08', 'AGEAS Seguros', '2023-08-10', 1, '13-24-GD');
 
 -- --------------------------------------------------------
 
@@ -81,20 +81,22 @@ INSERT INTO `insurance` (`policy`, `expiry_date`, `company`, `start_date`, `extr
 -- Estrutura da tabela `person`
 --
 
-CREATE TABLE `person` (
-  `nif` int(9) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `address` varchar(60) NOT NULL,
-  `b_date` date NOT NULL,
-  `password` varchar(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `person`
+(
+    `nif`      int(9)      NOT NULL,
+    `name`     varchar(45) NOT NULL,
+    `address`  varchar(60) NOT NULL,
+    `b_date`   date        NOT NULL,
+    `password` varchar(8)  NOT NULL,
+    `person_type` ENUM('customer', 'employee') NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
 
 --
 -- Extraindo dados da tabela `person`
 --
 
-INSERT INTO `person` (`nif`, `name`, `address`, `b_date`, `password`) VALUES
-(234554343, 'Luis Alberta Cina', 'Rua Sem Nome, Porta Inifinta', '2023-08-09', '123abc');
 
 -- --------------------------------------------------------
 
@@ -103,13 +105,13 @@ INSERT INTO `person` (`nif`, `name`, `address`, `b_date`, `password`) VALUES
 --
 
 CREATE TABLE `ticket` (
-  `nif` int(9) NOT NULL,
-  `plate` char(8) NOT NULL,
-  `date` date NOT NULL,
-  `expiry_date` date NOT NULL,
-  `value` double NOT NULL,
-  `reason` int(1) NOT NULL,
-  `paid` tinyint(1) NOT NULL DEFAULT 0
+                          `nif` int(9) NOT NULL,
+                          `plate` char(8) NOT NULL,
+                          `date` date NOT NULL,
+                          `expiry_date` date NOT NULL,
+                          `value` double NOT NULL,
+                          `reason` int(1) NOT NULL,
+                          `paid` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -117,7 +119,7 @@ CREATE TABLE `ticket` (
 --
 
 INSERT INTO `ticket` (`nif`, `plate`, `date`, `expiry_date`, `value`, `reason`, `paid`) VALUES
-(234554343, '13-24-GD', '2023-08-23', '2025-08-08', 234.77, 3, 0);
+    (234554343, '13-24-GD', '2023-08-23', '2025-08-08', 234.77, 3, 0);
 
 -- --------------------------------------------------------
 
@@ -126,14 +128,14 @@ INSERT INTO `ticket` (`nif`, `plate`, `date`, `expiry_date`, `value`, `reason`, 
 --
 
 CREATE TABLE `vehicle` (
-  `brand` varchar(45) NOT NULL,
-  `model` varchar(45) NOT NULL,
-  `color` varchar(45) NOT NULL,
-  `registration_date` date NOT NULL,
-  `plate` char(8) NOT NULL,
-  `vin` char(17) NOT NULL,
-  `category` int(1) NOT NULL,
-  `nif` int(9) NOT NULL
+                           `brand` varchar(45) NOT NULL,
+                           `model` varchar(45) NOT NULL,
+                           `color` varchar(45) NOT NULL,
+                           `registration_date` date NOT NULL,
+                           `plate` char(8) NOT NULL,
+                           `vin` char(17) NOT NULL,
+                           `category` int(1) NOT NULL,
+                           `nif` int(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -141,7 +143,7 @@ CREATE TABLE `vehicle` (
 --
 
 INSERT INTO `vehicle` (`brand`, `model`, `color`, `registration_date`, `plate`, `vin`, `category`, `nif`) VALUES
-('Mercedes Benz', 'A8', 'Black', '2023-07-13', '13-24-GD', '23423424FSS', 1, 234554343);
+    ('Mercedes Benz', 'A8', 'Black', '2023-07-13', '13-24-GD', '23423424FSS', 1, 234554343);
 
 --
 -- Índices para tabelas despejadas
@@ -151,43 +153,43 @@ INSERT INTO `vehicle` (`brand`, `model`, `color`, `registration_date`, `plate`, 
 -- Índices para tabela `customer`
 --
 ALTER TABLE `customer`
-  ADD PRIMARY KEY (`driver_license_number`),
-  ADD UNIQUE KEY `nif` (`nif`),
-  ADD UNIQUE KEY `nif_2` (`nif`);
+    ADD PRIMARY KEY (`driver_license_number`),
+    ADD UNIQUE KEY `nif` (`nif`),
+    ADD UNIQUE KEY `nif_2` (`nif`);
 
 --
 -- Índices para tabela `employee`
 --
 ALTER TABLE `employee`
-  ADD UNIQUE KEY `nif` (`nif`);
+    ADD UNIQUE KEY `nif` (`nif`);
 
 --
 -- Índices para tabela `insurance`
 --
 ALTER TABLE `insurance`
-  ADD PRIMARY KEY (`policy`),
-  ADD KEY `Plate_Insurance_FK` (`plate`);
+    ADD PRIMARY KEY (`policy`),
+    ADD KEY `Plate_Insurance_FK` (`plate`);
 
 --
 -- Índices para tabela `person`
 --
 ALTER TABLE `person`
-  ADD PRIMARY KEY (`nif`);
+    ADD PRIMARY KEY (`nif`);
 
 --
 -- Índices para tabela `ticket`
 --
 ALTER TABLE `ticket`
-  ADD PRIMARY KEY (`plate`,`nif`),
-  ADD KEY `Nif_Offender` (`nif`);
+    ADD PRIMARY KEY (`plate`,`nif`),
+    ADD KEY `Nif_Offender` (`nif`);
 
 --
 -- Índices para tabela `vehicle`
 --
 ALTER TABLE `vehicle`
-  ADD PRIMARY KEY (`plate`),
-  ADD UNIQUE KEY `vin` (`vin`),
-  ADD KEY `Owner_NIF` (`nif`);
+    ADD PRIMARY KEY (`plate`),
+    ADD UNIQUE KEY `vin` (`vin`),
+    ADD KEY `Owner_NIF` (`nif`);
 
 --
 -- Restrições para despejos de tabelas
@@ -197,34 +199,55 @@ ALTER TABLE `vehicle`
 -- Limitadores para a tabela `customer`
 --
 ALTER TABLE `customer`
-  ADD CONSTRAINT `nif_user` FOREIGN KEY (`nif`) REFERENCES `person` (`nif`) ON DELETE CASCADE ON UPDATE CASCADE;
+    ADD CONSTRAINT `nif_user` FOREIGN KEY (`nif`) REFERENCES `person` (`nif`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limitadores para a tabela `employee`
 --
 ALTER TABLE `employee`
-  ADD CONSTRAINT `nif_employee` FOREIGN KEY (`nif`) REFERENCES `person` (`nif`) ON DELETE CASCADE ON UPDATE CASCADE;
+    ADD CONSTRAINT `nif_employee` FOREIGN KEY (`nif`) REFERENCES `person` (`nif`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limitadores para a tabela `insurance`
 --
 ALTER TABLE `insurance`
-  ADD CONSTRAINT `Plate_Insurance_FK` FOREIGN KEY (`plate`) REFERENCES `vehicle` (`plate`) ON DELETE CASCADE ON UPDATE CASCADE;
+    ADD CONSTRAINT `Plate_Insurance_FK` FOREIGN KEY (`plate`) REFERENCES `vehicle` (`plate`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limitadores para a tabela `ticket`
 --
 ALTER TABLE `ticket`
-  ADD CONSTRAINT `Nif_Offender` FOREIGN KEY (`nif`) REFERENCES `customer` (`nif`),
-  ADD CONSTRAINT `Plate_Number_Ticket_FK` FOREIGN KEY (`plate`) REFERENCES `vehicle` (`plate`);
+    ADD CONSTRAINT `Nif_Offender` FOREIGN KEY (`nif`) REFERENCES `customer` (`nif`),
+    ADD CONSTRAINT `Plate_Number_Ticket_FK` FOREIGN KEY (`plate`) REFERENCES `vehicle` (`plate`);
 
 --
 -- Limitadores para a tabela `vehicle`
 --
 ALTER TABLE `vehicle`
-  ADD CONSTRAINT `Owner_NIF` FOREIGN KEY (`nif`) REFERENCES `customer` (`nif`) ON DELETE CASCADE ON UPDATE CASCADE;
+    ADD CONSTRAINT `Owner_NIF` FOREIGN KEY (`nif`) REFERENCES `customer` (`nif`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- Trigger to add Employee or Customer based on the flag in Person
+
+
+CREATE TRIGGER add_employee_or_customer
+    AFTER INSERT ON person
+    FOR EACH ROW
+BEGIN
+    IF NEW.person_type = 'EMPLOYEE' THEN
+        INSERT INTO employee (nif, access_level)
+        VALUES (NEW.nif, 0); -- Assuming default access_level is 0, you can change this value as needed
+    ELSEIF NEW.person_type = 'CUSTOMER' THEN
+        INSERT INTO customer (driver_license_number, license_type, start_date, expiry_date, nif)
+        VALUES (NEW.driver_license_number, NEW.license_type, NEW.start_date, NEW.expiry_date, NEW.nif);
+    END IF;
+END;
+
+//
+
+DELIMITER ;
+
---
+++ src/enumssql/PersonsEnum.java
@@ -0,0 +1,79 @@
+package enumssql;
+
+public enum PersonsEnum {
+    // constants for the variables of the table Person
+    TABLE_PERSON,
+    COLUMN_NIF,
+    COLUMN_NAME,
+    COLUMN_BIRTH_DATE,
+    COLUMN_PWD,
+    COLUMN_ADDRESS,
+    COLUMN_PERSON_TYPE_EMPLOYEE,
+    COLUMN_PERSON_TYPE_CUSTOMER,
+
+    //contants for the varibles of the table Customer
+    TABLE_CUSTOMER,
+    COLUMN_CUSTOMER_DRIVER_LICENSE,
+    COLUMN_CUSTOMER_LICENSE_TYPE,
+    COLUMN_CUSTOMER_START_DATE,
+    COLUMN_CUSTOMER_EXPIRATION_DATE,
+
+    //Constants for the variables of the table Employee
+    TABLE_EMPLOYEE,
+    COLUMN_EMPLOYEE_ACCESS_LEVEL, INSERT_INTO_PERSON, INSERT_INTO_EMPLOYEE, INSERT_INTO_CUSTOMER;
+
+    //Method to get the string of the enum
+    public static String getString(PersonsEnum pes) {
+        String s = switch (pes) {
+            case TABLE_PERSON -> "person";
+            case COLUMN_NIF -> "nif";
+            case COLUMN_NAME -> "name";
+            case COLUMN_BIRTH_DATE -> "birth_date";
+            case COLUMN_PWD -> "pwd";
+            case COLUMN_ADDRESS -> "address";
+            case TABLE_CUSTOMER -> "customer";
+            case COLUMN_PERSON_TYPE_CUSTOMER -> "CUSTOMER";
+            case COLUMN_CUSTOMER_DRIVER_LICENSE -> "driver_license";
+            case COLUMN_CUSTOMER_LICENSE_TYPE -> "license_type";
+            case COLUMN_CUSTOMER_START_DATE -> "start_date";
+            case COLUMN_CUSTOMER_EXPIRATION_DATE -> "expiration_date";
+            case TABLE_EMPLOYEE -> "employee";
+            case COLUMN_PERSON_TYPE_EMPLOYEE -> "EMPLOYEE";
+            case COLUMN_EMPLOYEE_ACCESS_LEVEL -> "access_level";
+
+            case INSERT_INTO_PERSON -> "INSERT INTO " + getString(PersonsEnum.TABLE_PERSON) + '('
+                    + getString(PersonsEnum.COLUMN_NIF) + ", " +
+                    getString(PersonsEnum.COLUMN_NAME) + ", " +
+                    getString(PersonsEnum.COLUMN_BIRTH_DATE) + ", " +
+                    getString(PersonsEnum.COLUMN_PWD) + ", " +
+                    getString(PersonsEnum.COLUMN_ADDRESS) + ") VALUES(?, ?, ?, ?, ?)";
+
+            case INSERT_INTO_EMPLOYEE -> "INSERT INTO " + getString(PersonsEnum.TABLE_EMPLOYEE) + '('
+                    + getString(PersonsEnum.COLUMN_NIF) + ", " +
+                    getString(PersonsEnum.COLUMN_NAME) + ", " +
+                    getString(PersonsEnum.COLUMN_BIRTH_DATE) + ", " +
+                    getString(PersonsEnum.COLUMN_PWD) + ", " +
+                    getString(PersonsEnum.COLUMN_ADDRESS) + ", " +
+                    getString(PersonsEnum.COLUMN_PERSON_TYPE_EMPLOYEE) +
+                    getString(PersonsEnum.COLUMN_EMPLOYEE_ACCESS_LEVEL) + ") VALUES(?, ?, ?, ?, ?, ?)";
+            case INSERT_INTO_CUSTOMER -> "INSERT INTO " + getString(PersonsEnum.TABLE_CUSTOMER) + '('
+                    + getString(PersonsEnum.COLUMN_NIF) + ", " +
+                    getString(PersonsEnum.COLUMN_NAME) + ", " +
+                    getString(PersonsEnum.COLUMN_BIRTH_DATE) + ", " +
+                    getString(PersonsEnum.COLUMN_PWD) + ", " +
+                    getString(PersonsEnum.COLUMN_ADDRESS) + ", " +
+                    getString(PersonsEnum.COLUMN_PERSON_TYPE_CUSTOMER) +
+                    getString(PersonsEnum.COLUMN_CUSTOMER_DRIVER_LICENSE) + ", " +
+                    getString(PersonsEnum.COLUMN_CUSTOMER_LICENSE_TYPE) + ", " +
+                    getString(PersonsEnum.COLUMN_CUSTOMER_START_DATE) + ", " +
+                    getString(PersonsEnum.COLUMN_CUSTOMER_EXPIRATION_DATE) + ") VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+
+            default -> throw new IllegalArgumentException("No such column or operation for person table");
+
+        };
+        return s;
+    }
+
+
+}
